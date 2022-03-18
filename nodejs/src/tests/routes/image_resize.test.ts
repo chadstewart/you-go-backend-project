92fdab97661798fbcb076dbe5e2fa133e8b62a02
Chad Stewart <chadrhonanstewart@gmail.com>
2022-03-18 17:48:17 -0500

    Putting routes tests in their own file.

--- nodejs/src/tests/routes/image_resize.test.ts
+++ nodejs/src/tests/routes/image_resize.test.ts
@@ -1,20 +1,6 @@
 import request from "supertest";
-import { app, secureServer as server } from "../app";
-import requestPayload from "./test_utils/test-payload";
-
-describe("Index Route", () => {
-  afterEach(done => {
-    server.close();
-    done();
-  });
-
-  it("It should get a 200 status code and get the message \'Server is live!\'", async () => {
-    const res = await request(app).get("/");
-
-    expect(res.statusCode).toEqual(200);
-    expect(res.text).toEqual("Server is live!");
-  });
-});
+import { app, secureServer as server } from "../../app";
+import requestPayload from "../test_utils/test-payload";
 
 describe("Images Resize Route", () => {
   afterEach(done => {
