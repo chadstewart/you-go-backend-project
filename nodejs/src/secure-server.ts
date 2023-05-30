d135de5d33ca4b30cbc9747bdf7825cb23b3004a
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-05-30 00:11:09 +0000

    Updated tooling for project. Cleaned up package.json, added tsx and removed nodemon.

--- nodejs/src/secure-server.ts
+++ nodejs/src/secure-server.ts
@@ -1,5 +1,14 @@
-import { secureServer } from "./app";
+import https from "https";
+import fs from "fs";
+import path from "path";
+import { app } from "./app";
 import logger from "./logger";
+
+//Enable https
+const secureServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, "../cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "../cert", "cert.pem"))
+}, app);
 
 const port = 3000;
 secureServer.listen(port, () => {
